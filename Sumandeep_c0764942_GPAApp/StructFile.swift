7d0fe9532ef94fe23f5b676d6f2c8aa04f85c3b7
Sumandeep2111 <sumandeep21111994@gmail.com>
2019-11-15 14:26:46 -0500

    add student data

---
+++ Sumandeep_c0764942_GPAApp/StructFile.swift
@@ -0,0 +1,17 @@
+//
+//  StructFile.swift
+//  Sumandeep_c0764942_GPAApp
+//
+//  Created by MacStudent on 2019-11-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+struct StudentsName {
+    
+    var firstname:String
+    var lastname:String
+    var studentid:String
+    static var StudentData = [StudentsName]()
+    
+}
