2d4539ad9fb622099564337c8c1f4015ecc1a56c
Sumandeep2111 <sumandeep21111994@gmail.com>
2019-11-15 15:44:06 -0500

    add alert

---
+++ Sumandeep_c0764942_GPAApp/controller/CalculateGPAViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalculateGPAViewController.swift
+//  Sumandeep_c0764942_GPAApp
+//
+//  Created by MacStudent on 2019-11-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class CalculateGPAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
